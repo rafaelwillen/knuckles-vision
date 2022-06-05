b5ffdaadf8f38fc3bc59c2bd25e9b9343c477e9a
Rafael Willen <rafaelpadre20@gmail.com>
2022-06-05 16:34:14 +0100

    🍻 remove some boilerplate code from electron

--- electron/index.ts
+++ electron/index.ts
@@ -1,19 +1,16 @@
 // Native
 import { join } from 'path';
-
-// Packages
-import { BrowserWindow, app, ipcMain, IpcMainEvent } from 'electron';
+import { BrowserWindow, app, ipcMain } from 'electron';
 import isDev from 'electron-is-dev';
 
-const height = 1024;
-const width = 1440;
+const windowHeight = 1024;
+const windowWidth = 1440;
 
 function createWindow() {
   // Create the browser window.
   const window = new BrowserWindow({
-    width,
-    height,
-    //  change to false to use AppBar
+    width: windowWidth,
+    height: windowHeight,
     frame: false,
     show: true,
     resizable: true,
@@ -32,17 +29,12 @@ function createWindow() {
   } else {
     window?.loadFile(url);
   }
-  // Open the DevTools.
-  // window.webContents.openDevTools();
 
   // For AppBar
   ipcMain.on('minimize', () => {
-    // eslint-disable-next-line no-unused-expressions
     window.isMinimized() ? window.restore() : window.minimize();
-    // or alternatively: win.isVisible() ? win.hide() : win.show()
   });
   ipcMain.on('maximize', () => {
-    // eslint-disable-next-line no-unused-expressions
     window.isMaximized() ? window.restore() : window.maximize();
   });
 
@@ -51,31 +43,13 @@ function createWindow() {
   });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow();
-
   app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit();
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-
-// listen the channel `message` and resend the received message to the renderer process
-ipcMain.on('message', (event: IpcMainEvent, message: any) => {
-  console.log(message);
-  setTimeout(() => event.sender.send('message', 'hi from electron'), 500);
 });
