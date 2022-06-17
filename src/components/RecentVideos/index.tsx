972fd8f248fb828956af131566fa1f3f7dbee75a
Rafael Willen <rafaelpadre20@gmail.com>
2022-06-17 17:47:40 +0100

    💄 style the text in thumbs

--- src/components/RecentVideos/index.tsx
+++ src/components/RecentVideos/index.tsx
@@ -5,8 +5,8 @@ import { Props } from "./type";
 const RecentVideos: React.FC<Props> = ({ videos }) => {
   return (
     <section className="p-5">
-      <h2 className="text-2xl">Videos Recentes</h2>
-      <div className="grid grid-rows-2  grid-cols-2  md:grid-cols-3 lg:grid-flow-col gap-6">
+      <h2 className="text-2xl mb-2">Videos Recentes</h2>
+      <div className="grid grid-rows-2  grid-cols-2  md:grid-cols-3 lg:grid-cols-4 gap-6">
         {videos.map((video) => (
           <VideoThumbnail video={video} />
         ))}
--- src/components/VideoThumbnail/index.tsx
+++ src/components/VideoThumbnail/index.tsx
@@ -4,11 +4,11 @@ import { Props } from "./type";
 const VideoThumbnail: React.FC<Props> = ({ video }) => {
   const { publicationDate, thumbnailImage, videoOwner, videoTitle } = video;
   return (
-    <div className="cursor-pointer">
+    <div className="cursor-pointer flex flex-col gap-1">
       <img src={thumbnailImage} alt={videoTitle} />
-      <h3>{videoTitle}</h3>
-      <p>{videoOwner}</p>
-      <p>{publicationDate.toLocaleDateString()}</p>
+      <h3 className="font-bold">{videoTitle}</h3>
+      <p className="font-normal text-sm text-grey-400">{videoOwner}</p>
+      <p className="text-sm">{publicationDate.toLocaleDateString()}</p>
     </div>
   );
 };
