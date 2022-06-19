b521ea011c742d07ecbb830f18e5010121361c09
Rafael Willen <rafaelpadre20@gmail.com>
2022-06-19 23:40:00 +0100

    ♻️ remove the sidebar button

--- src/components/Button/MenuButton/index.tsx
+++ src/components/Button/MenuButton/index.tsx
@@ -1,14 +1,27 @@
-import React from 'react'
-import { Props } from './types';
+import React from "react";
+import { Props } from "./types";
 
-const MenuButton: React.FC<Props> = ({ onClick = () => { }, text, icon: Icon, isSelected = false, textIsVisible = true }) => {
-    return (
-        <button onClick={onClick} className={`hover:bg-grey-300/10
-         w-full flex items-center py-5 px-6 gap-7 ${isSelected ? "bg-grey-300/10" : "bg-dark-700"}`} >
-            {Icon}
-            {textIsVisible && <span className='flex-1 text-left text-xl'>{text}</span>}
-        </button >
-    )
-}
+const MenuButton: React.FC<Props> = ({
+  onClick = () => {},
+  text,
+  icon: Icon,
+  isSelected = false,
+  textIsVisible = true,
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`hover:bg-grey-300/10
+         w-full flex items-center py-5 px-6 gap-7 ${
+           isSelected ? "bg-grey-300/10" : "bg-dark-700"
+         }`}
+    >
+      {Icon}
+      {textIsVisible && (
+        <span className="flex-1 text-left text-base">{text}</span>
+      )}
+    </button>
+  );
+};
 
 export default MenuButton;
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,49 +1,73 @@
-import React, { useState } from 'react'
-import { MdMenu, MdOutlineHome, MdPersonOutline, MdFavoriteBorder, MdOutlineFolder, MdOutlineAdd } from 'react-icons/md';
-import MenuButton from '../Button/MenuButton';
+import React from "react";
+import {
+  MdOutlineHome,
+  MdPersonOutline,
+  MdFavoriteBorder,
+  MdOutlineFolder,
+  MdOutlineAdd,
+} from "react-icons/md";
+import MenuButton from "../Button/MenuButton";
 
 const Sidebar = () => {
-    const ICON_SIZE = 30;
-    const [isSidebarVisible, setSidebarVisible] = useState(true);
+  const ICON_SIZE = 30;
 
-    const fakeGroups = [
-        "Group 1",
-        "Super Group 1",
-        "Super DUPER Group 1",
-        "The first Group 1",
-        "The 0s",
-        "THE SUPER MEGA DUPER GROUP 1!",
-        "MEMES",
-        "F1",
-    ]
+  const fakeGroups = [
+    "Group 1",
+    "Super Group 1",
+    "Super DUPER Group 1",
+    "The first Group 1",
+    "The 0s",
+    "THE SUPER MEGA DUPER GROUP 1!",
+    "MEMES",
+    "F1",
+  ];
 
-    return (
-        <aside className={`w-1/4 max-w-xs min-h-screen
-        ${!isSidebarVisible && "w-[80px]"}`}
-            style={{ background: "linear-gradient(0deg, #242424 88%, rgba(218, 0, 0, 0.77) 110%)" }}>
-            <section className='flex justify-center items-center py-10 px-6 gap-8'>
-                <MdMenu size={ICON_SIZE} onClick={() => setSidebarVisible(!isSidebarVisible)} />
-                {isSidebarVisible && <h1 className='font-normal text-2xl text-white'>Knuckles Vision</h1>}
-            </section>
-            <section className='flex flex-col gap-4'>
-                <MenuButton isSelected icon={<MdOutlineHome size={ICON_SIZE} />} text="Início" textIsVisible={isSidebarVisible} />
-                <MenuButton icon={<MdPersonOutline size={ICON_SIZE} />} text="Minha Conta" textIsVisible={isSidebarVisible} />
-                <MenuButton icon={<MdFavoriteBorder size={ICON_SIZE} />} text="Favoritos" textIsVisible={isSidebarVisible} />
-                <MenuButton icon={<MdOutlineFolder size={ICON_SIZE} />} text="Meu Conteúdo" textIsVisible={isSidebarVisible} />
-            </section>
-            {isSidebarVisible && <section className='flex flex-col border-t-2 border-t-grey-700 py-7 px-6 gap-9'>
-                <div className='flex items-center'>
-                    <span className='flex-1 text-xl'>Meus Grupos</span>
-                    <MdOutlineAdd className='cursor-pointer' size={ICON_SIZE} />
-                </div>
-                <div className='flex flex-col gap-3 text-base'>
-                    {fakeGroups.map((group, index) => <span key={index} className="hover:underline cursor-pointer" >
-                        {group}
-                    </span>)}
-                </div>
-            </section>}
-        </aside>
-    )
-}
+  return (
+    <aside
+      className="w-1/5 min-h-screen"
+      style={{
+        background:
+          "linear-gradient(0deg, #242424 88%, rgba(218, 0, 0, 0.77) 110%)",
+      }}
+    >
+      <section className="flex justify-center items-center py-10 px-6 gap-8">
+        <h1 className="font-normal text-xl text-white">Knuckles Vision</h1>
+      </section>
+      <section className="flex flex-col gap-4">
+        <MenuButton
+          isSelected
+          icon={<MdOutlineHome size={ICON_SIZE} />}
+          text="Início"
+        />
+        <MenuButton
+          icon={<MdPersonOutline size={ICON_SIZE} />}
+          text="Minha Conta"
+        />
+        <MenuButton
+          icon={<MdFavoriteBorder size={ICON_SIZE} />}
+          text="Favoritos"
+        />
+        <MenuButton
+          icon={<MdOutlineFolder size={ICON_SIZE} />}
+          text="Meu Conteúdo"
+        />
+      </section>
+
+      <section className="flex flex-col border-t-2 border-t-grey-700 py-7 px-6 gap-9">
+        <div className="flex items-center">
+          <span className="flex-1 text-xl">Meus Grupos</span>
+          <MdOutlineAdd className="cursor-pointer" size={ICON_SIZE} />
+        </div>
+        <div className="flex flex-col gap-3 text-base">
+          {fakeGroups.map((group, index) => (
+            <span key={index} className="hover:underline cursor-pointer">
+              {group}
+            </span>
+          ))}
+        </div>
+      </section>
+    </aside>
+  );
+};
 
 export default Sidebar;
