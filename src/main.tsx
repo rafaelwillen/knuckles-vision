a9b840b3fe4281db5d26c6f594e820b08aa53f39
Rafael Willen <rafaelpadre20@gmail.com>
2022-06-30 00:14:14 +0100

    ✨ create react query provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import AuthProvider from "./context/AuthContext";
@@ -15,40 +16,44 @@ import { RoutesEnum } from "./routes/RoutesEnum";
 import SignUp from "./routes/SignUp";
 import VideoPlayer from "./routes/VideoPlayer";
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-          <Route path={RoutesEnum.Root} element={<App />}>
-            {/* Public Routes */}
-            <Route path={RoutesEnum.Home} element={<Home />} />
-            <Route path={RoutesEnum.VideoPlayer} element={<VideoPlayer />} />
-            <Route path={RoutesEnum.MusicPlayer} element={<MusicPlayer />} />
-            <Route path={RoutesEnum.Login} element={<Login />} />
-            <Route path={RoutesEnum.SignUp} element={<SignUp />} />
-            <Route path={RoutesEnum.Radio} element={<Radio />} />
-            {/* Protected Routes */}
-            <Route
-              path={RoutesEnum.Account}
-              element={
-                <ProtectedRoute>
-                  <Account />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path={RoutesEnum.CreateGroup}
-              element={
-                <ProtectedRoute>
-                  <CreateGroup />
-                </ProtectedRoute>
-              }
-            />
-          </Route>
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <AuthProvider>
+          <Routes>
+            <Route path={RoutesEnum.Root} element={<App />}>
+              {/* Public Routes */}
+              <Route path={RoutesEnum.Home} element={<Home />} />
+              <Route path={RoutesEnum.VideoPlayer} element={<VideoPlayer />} />
+              <Route path={RoutesEnum.MusicPlayer} element={<MusicPlayer />} />
+              <Route path={RoutesEnum.Login} element={<Login />} />
+              <Route path={RoutesEnum.SignUp} element={<SignUp />} />
+              <Route path={RoutesEnum.Radio} element={<Radio />} />
+              {/* Protected Routes */}
+              <Route
+                path={RoutesEnum.Account}
+                element={
+                  <ProtectedRoute>
+                    <Account />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path={RoutesEnum.CreateGroup}
+                element={
+                  <ProtectedRoute>
+                    <CreateGroup />
+                  </ProtectedRoute>
+                }
+              />
+            </Route>
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
